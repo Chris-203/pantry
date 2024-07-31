566905a306b62da1b4e04d595871e66263999ba6
Christopher Cortazo <cortazo.christopher@gmail.com>
2024-07-31 00:06:55 -0400

    changed settings

--- my-pantry/firebase.js
+++ my-pantry/firebase.js
@@ -1,14 +1,15 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { process } from "node";
 
-// Your web app's Firebase configuration
+// Your web app's Firebase configuration from environment variables
 const firebaseConfig = {
-  apiKey: "AIzaSyBqC3MSUn1II6qurSxmlDT8pIf1wY6aWZA",
-  authDomain: "mypantryapp-3569a.firebaseapp.com",
-  projectId: "mypantryapp-3569a",
-  storageBucket: "mypantryapp-3569a.appspot.com",
-  messagingSenderId: "663898581689",
-  appId: "1:663898581689:web:e310f077fe83dde5994968",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
