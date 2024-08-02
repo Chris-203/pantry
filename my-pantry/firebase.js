66b0c2065201c82ed49d7515802b053fb837e925
Christopher Cortazo <cortazo.christopher@gmail.com>
2024-08-02 16:14:57 -0400

    udpdated env & added .env.local

--- my-pantry/firebase.js
+++ my-pantry/firebase.js
@@ -1,15 +1,15 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { apiKey, authDomain, projectId, storageBucket, messagingSenderId, appId } from './firebaseConfig';
+// import { apiKey, authDomain, projectId, storageBucket, messagingSenderId, appId } from './firebaseConfig';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-    apiKey,
-    authDomain,
-    projectId,
-    storageBucket,
-    messagingSenderId,
-    appId,
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
   };
 
 // Initialize Firebase
@@ -17,3 +17,5 @@ const app = initializeApp(firebaseConfig);
 const firestore = getFirestore(app);
 const auth = getAuth(app);
 export {firebaseConfig, app, firestore, auth };
+
+
---
+++ my-pantry/firebaseConfig.js
@@ -0,0 +1,9 @@
+// firebaseConfig.js
+const apiKey = "AIzaSyAc_jOdRA1MLVuz6quv9mXSZNsPDhIF3Hw";
+const authDomain = "mypantryapp-3569a.firebaseapp.com";
+const projectId = "mypantryapp-3569a";
+const storageBucket = "mypantryapp-3569a.appspot.com";
+const messagingSenderId = "663898581689";
+const appId = "1:663898581689:web:e310f077fe83dde5994968";
+
+export { apiKey, authDomain, projectId, storageBucket, messagingSenderId, appId };
