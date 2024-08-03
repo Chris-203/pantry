fc19251fc8ddba3c9b47624afa97b25e49fa9f6e
Christopher Cortazo <cortazo.christopher@gmail.com>
2024-08-03 00:44:37 -0400

    added signup modal and assigned names to account created in the app

--- my-pantry/app/page.js
+++ my-pantry/app/page.js
@@ -1,10 +1,20 @@
 "use client";
-
+import {
+  doc,
+  setDoc,
+} from "firebase/firestore";
 import React, { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
-import { GoogleAuthProvider, signInWithPopup, signInWithEmailAndPassword, createUserWithEmailAndPassword } from "firebase/auth";
-import { Button, TextField, Box, Typography } from "@mui/material";
-import { auth } from "@/firebase";
+import {
+  GoogleAuthProvider,
+  signInWithPopup,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+} from "firebase/auth";
+import { Button, TextField, Box, Typography, Dialog, DialogActions, DialogContent, DialogTitle } from "@mui/material";
+import { SnackbarProvider, useSnackbar } from "notistack";
+import { firestore, auth } from "@/firebase";
+import { updateProfile } from "firebase/auth";
 
 const googleProvider = new GoogleAuthProvider();
 
@@ -13,6 +23,9 @@ const Auth = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [loading, setLoading] = useState(true);
+  const [openSignUpModal, setOpenSignUpModal] = useState(false);
+  const [fullName, setFullName] = useState("");
+  const { enqueueSnackbar } = useSnackbar();
 
   const checkAuthStatus = async () => {
     const user = auth.currentUser;
@@ -46,10 +59,54 @@ const Auth = () => {
     }
   };
 
-  const handleSignUp = async () => {
+  // const handleSignUp = async () => {
+  //   try {
+  //     const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+  //     const user = userCredential.user;
+  
+  //     // Prompt for the user's full name
+  //     const fullName = prompt("Please enter your full name");
+  
+  //     if (fullName) {
+  //       // Update user profile with the full name
+  //       await user.updateProfile({ displayName: fullName });
+  
+  //       // Save user data to Firestore
+  //       await setDoc(doc(firestore, `users/${user.uid}`), { name: fullName });
+  
+  //       router.push("/auth");
+  //     } else {
+  //       alert("Full name is required!");
+  //     }
+  //   } catch (error) {
+  //     console.error("Sign Up Error:", error);
+  //     alert(`Sign Up Error: ${error.message}`);
+  //   }
+  // };
+
+  const handleSignUpClick = () => {
+    setOpenSignUpModal(true);
+  };
+
+  const handleSignUpSubmit = async () => {
     try {
-      await createUserWithEmailAndPassword(auth, email, password);
-      router.push("/auth");
+      const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+      const user = userCredential.user;
+  
+      // Prompt for the user's full name
+      const fullName = prompt("Please enter your full name");
+  
+      if (fullName) {
+        // Update user profile with the full name
+        await updateProfile(user,{ displayName: fullName });
+  
+        // Save user data to Firestore
+        await setDoc(doc(firestore, `users/${user.uid}`), { name: fullName });
+  
+        router.push("/auth");
+      } else {
+        alert("Full name is required!");
+      }
     } catch (error) {
       console.error("Sign Up Error:", error);
       alert(`Sign Up Error: ${error.message}`);
@@ -61,37 +118,94 @@ const Auth = () => {
   }
 
   return (
-    <Box display="flex" flexDirection="column" alignItems="center" justifyContent="center" height="100vh">
-      <Typography variant="h4" gutterBottom>
-        Sign In
-      </Typography>
-      <Button variant="contained" onClick={handleGoogleSignIn}>
-        Sign in with Google
-      </Button>
-      <Typography variant="h6" gutterBottom>
-        or
-      </Typography>
-      <TextField
-        label="Email"
-        type="email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-        margin="normal"
-      />
-      <TextField
-        label="Password"
-        type="password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-        margin="normal"
-      />
-      <Button variant="contained" onClick={handleEmailSignIn}>
-        Sign In with Email
-      </Button>
-      <Button variant="outlined" onClick={handleSignUp} style={{ marginTop: "16px" }}>
-        Sign Up
-      </Button>
-    </Box>
+    <SnackbarProvider maxSnack={3}>
+      <Box
+        display="flex"
+        flexDirection="column"
+        alignItems="center"
+        justifyContent="center"
+        height="100vh"
+        sx={{ marginTop: "-50px" }}
+      >
+        <img
+          src="/images/logo.png"
+          alt="Logo"
+          style={{ height: "30%", width: "20%" }}
+        />
+
+        <Typography variant="h4" gutterBottom sx={{ mb: 5 }}>
+          Sign In
+        </Typography>
+        <Button variant="contained" onClick={handleGoogleSignIn} sx={{ mb: 2 }}>
+          Sign in with Google
+        </Button>
+        <Typography variant="h6">or</Typography>
+        <TextField
+          label="Email"
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          margin="normal"
+        />
+        <TextField
+          label="Password"
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          sx={{ mb: 3 }}
+        />
+        <Button variant="contained" onClick={handleEmailSignIn}>
+          Sign In with Email
+        </Button>
+        <Button
+          variant="outlined"
+          onClick={handleSignUpClick}
+          style={{ marginTop: "16px" }}
+        >
+          Sign Up
+        </Button>
+
+        <Dialog open={openSignUpModal} onClose={() => setOpenSignUpModal(false)}>
+          <DialogTitle>Sign Up</DialogTitle>
+          <DialogContent>
+            <TextField
+              autoFocus
+              margin="dense"
+              label="Full Name"
+              type="text"
+              fullWidth
+              variant="outlined"
+              value={fullName}
+              onChange={(e) => setFullName(e.target.value)}
+              sx={{ mb: 2 }}
+            />
+            <TextField
+              margin="dense"
+              label="Email"
+              type="email"
+              fullWidth
+              variant="outlined"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              sx={{ mb: 2 }}
+            />
+            <TextField
+              margin="dense"
+              label="Password"
+              type="password"
+              fullWidth
+              variant="outlined"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={() => setOpenSignUpModal(false)}>Cancel</Button>
+            <Button onClick={handleSignUpSubmit}>Sign Up</Button>
+          </DialogActions>
+        </Dialog>
+      </Box>
+    </SnackbarProvider>
   );
 };
 
